d9e5828beec6da4a3928a1dbb5875b2d9e548b6f
Hussain Abbasi <hnabbasi@outlook.com>
2022-09-10 00:01:27 -0500

    Added alerts

---
+++ XamWxApp/XamWxApp/Alert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace XamWxApp.Models
+{
+    public class Alert
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string AreaDesc { get; set; }
+        public DateTimeOffset Sent { get; set; }
+        public DateTimeOffset Effective { get; set; }
+        public DateTimeOffset Expires { get; set; }
+        public string Status { get; set; }
+        public string Severity { get; set; }
+        public string Certainty { get; set; }
+        public string Urgency { get; set; }
+        public string Event { get; set; }
+        public string SenderName { get; set; }
+        public string Headline { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+        public string Response { get; set; }
+    }
+}
--- XamWxApp/XamWxApp/MainPage.xaml.cs
+++ XamWxApp/XamWxApp/MainPage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using XamWxApp.Models;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace XamWxApp
 {
@@ -13,6 +13,126 @@ namespace XamWxApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void GetAlertsButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            AlertsCount.Text = "";
+            AlertsList.ItemsSource = null;
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(new Uri($"https://api.weather.gov/alerts/active/area/{StateCode.Text.ToUpper()}"));
+            var alerts = GetAlertsByZones(JObject.Parse(response));
+
+            if (alerts.Length > 0)
+                AlertsCount.Text = $"Found {alerts.Length} active alerts";
+            else
+                AlertsCount.Text = "No active alerts found";
+
+            AlertsList.ItemsSource = alerts;
+        }
+
+        Alert[] GetAlertsByZones(JObject json)
+        {
+            try
+            {
+                var features = json["features"];
+
+                if (features == null || features.Type == JTokenType.Null || features.Count() == 0)
+                {
+                    Console.WriteLine($">>> No alerts found.");
+                    return new Alert[0];
+                }
+                var count = features.Count();
+
+                var alerts = new Alert[count];
+#if DEBUG
+                Console.WriteLine($">>> Parsing {features.Count()} alerts...");
+#endif
+
+                for (int i = 0; i < count; i++)
+                {
+                    var prop = features[i]?["properties"];
+
+                    if (prop == null || prop.Type == JTokenType.Null)
+                        return new Alert[0];
+
+                    var alert = new Alert();
+
+                    var id = prop["@id"];
+                    if (id != null && id.Type != JTokenType.Null)
+                        alert.Id = id.Value<string>();
+
+                    var type = prop["@type"];
+                    if (type != null && type.Type != JTokenType.Null)
+                        alert.Type = type.Value<string>();
+
+                    var areaDesc = prop["areaDesc"];
+                    if (areaDesc != null && areaDesc.Type != JTokenType.Null)
+                        alert.AreaDesc = areaDesc.Value<string>();
+
+                    var sent = prop["sent"];
+                    if (sent != null && sent.Type != JTokenType.Null)
+                        alert.Sent = DateTimeOffset.Parse(sent.Value<string>());
+
+                    var effective = prop["effective"];
+                    if (effective != null && effective.Type != JTokenType.Null)
+                        alert.Effective = DateTimeOffset.Parse(effective.Value<string>());
+
+                    var expires = prop["expires"];
+                    if (expires != null && expires.Type != JTokenType.Null)
+                        alert.Expires = DateTimeOffset.Parse(expires.Value<string>());
+
+                    var status = prop["status"];
+                    if (status != null && status.Type != JTokenType.Null)
+                        alert.Status = status.Value<string>();
+
+                    var severity = prop["severity"];
+                    if (severity != null && severity.Type != JTokenType.Null)
+                        alert.Severity = severity.Value<string>();
+
+                    var certainty = prop["certainty"];
+                    if (certainty != null && certainty.Type != JTokenType.Null)
+                        alert.Certainty = certainty.Value<string>();
+
+                    var urgency = prop["urgency"];
+                    if (urgency != null && urgency.Type != JTokenType.Null)
+                        alert.Urgency = urgency.Value<string>();
+
+                    var @event = prop["event"];
+                    if (@event != null && @event.Type != JTokenType.Null)
+                        alert.Event = @event.Value<string>();
+
+                    var senderName = prop["senderName"];
+                    if (senderName != null && senderName.Type != JTokenType.Null)
+                        alert.SenderName = senderName.Value<string>();
+
+                    var headline = prop["headline"];
+                    if (headline != null && headline.Type != JTokenType.Null)
+                        alert.Headline = headline.Value<string>();
+
+                    var description = prop["description"];
+                    if (description != null && description.Type != JTokenType.Null)
+                        alert.Description = description.Value<string>();
+
+                    var instruction = prop["instruction"];
+                    if (instruction != null && instruction.Type != JTokenType.Null)
+                        alert.Instruction = instruction.Value<string>();
+
+                    var response = prop["response"];
+                    if (response != null && response.Type != JTokenType.Null)
+                        alert.Response = response.Value<string>();
+
+                    alerts[i] = alert;
+                }
+
+                Console.WriteLine($">>> Found {count} alerts");
+                return alerts;
+            }
+            catch (Exception ex)
+            {
+                return new Alert[0];
+            }
         }
     }
 }
